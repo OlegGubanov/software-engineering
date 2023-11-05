2520e5374c28caac666b9a27001356c2311bb30e
s17v3r <o-gybanov@mail.ru>
2023-11-05 23:54:50 +0500

    add streamlit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from transformers import pipeline
+import streamlit as st
 
-translator_ru_to_en = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
-word = translator_ru_to_en(input())[0]['translation_text']
-print(word)
+
+@st.cache_resource
+def load_model():
+    return pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
+
+
+model = load_model()
+text = st.text_input(label='Введите текст для перевода')
+if text:
+    translation = model(text)
+    st.write(translation[0]['translation_text'])
